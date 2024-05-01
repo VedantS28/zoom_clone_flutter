0f4d25baee9ca329011195a3c7b3e2cecc02d2b5
VedantS28 <vedantshrivastava286@gmail.com>
2024-05-01 12:39:15 +0530

    auth method updated

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -6,14 +6,10 @@ class FirebaseAuthMethods {
   final FirebaseAuth _auth;
   FirebaseAuthMethods(this._auth);
 
-
   User get user => _auth.currentUser!;
 
-  // STATE PERSISTENCE STREAM
   Stream<User?> get authState => FirebaseAuth.instance.authStateChanges();
 
-
-  // EMAIL SIGN UP
   Future<void> signUpWithEmail({
     required String email,
     required String password,
@@ -26,17 +22,15 @@ class FirebaseAuthMethods {
       );
       await sendEmailVerification(context);
     } on FirebaseAuthException catch (e) {
-      // if you want to display your own custom error message
       if (e.code == 'weak-password') {
         print('The password provided is too weak.');
       } else if (e.code == 'email-already-in-use') {
         print('The account already exists for that email.');
       }
-      showSnackBar(
-          context, e.message!); // Displaying the usual firebase error message
+      showSnackBar(context, e.message!);
     }
   }
-  // EMAIL LOGIN
+
   Future<void> loginWithEmail({
     required String email,
     required String password,
@@ -49,21 +43,18 @@ class FirebaseAuthMethods {
       );
       if (!user.emailVerified) {
         await sendEmailVerification(context);
-        // restrict access to certain things using provider
-        // transition to another page instead of home screen
       }
     } on FirebaseAuthException catch (e) {
-      showSnackBar(context, e.message!); // Displaying the error message
+      showSnackBar(context, e.message!);
     }
   }
 
-  // EMAIL VERIFICATION
   Future<void> sendEmailVerification(BuildContext context) async {
     try {
       _auth.currentUser!.sendEmailVerification();
       showSnackBar(context, 'Email verification sent!');
     } on FirebaseAuthException catch (e) {
-      showSnackBar(context, e.message!); // Display error message
+      showSnackBar(context, e.message!);
     }
   }
 }
